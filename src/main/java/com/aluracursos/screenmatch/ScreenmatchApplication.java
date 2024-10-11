2897fbe3d417c904641fa1dff6e6c6395e8e8cca
Jeffrey MC <jeffmc@duck.com>
2024-10-11 13:52:54 -0600

    Iterando entre todos los episodios de todas las temporadas

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.aluracursos.screenmatch;
 
 import com.aluracursos.screenmatch.model.DatosEpisodio;
 import com.aluracursos.screenmatch.model.DatosSerie;
+import com.aluracursos.screenmatch.model.DatosTemporadas;
 import com.aluracursos.screenmatch.service.ConsumoAPI;
 import com.aluracursos.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication  implements CommandLineRunner {
@@ -29,5 +33,14 @@ public class ScreenmatchApplication  implements CommandLineRunner {
 		DatosEpisodio episodios = conversor.obtenerDatos(json, DatosEpisodio.class);
 
 		System.out.println(episodios);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+			json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=how+i+met+your+mother&Season=" + i + "&apikey=60afd634");
+			var datosTemporadas = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporadas);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
